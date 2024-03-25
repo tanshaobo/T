645ec2107c09503cbb8581b2aee02b118c688ffb
tanshaobo <18235140339@163.com>
2024-03-26 00:18:30 +0800

    fix: ts重载功能真的难懂

--- ts/ts-02.ts
+++ ts/ts-02.ts
@@ -2,7 +2,7 @@
  * @Author: tanshaobo
  * @Date: 2022-07-02 21:56:01
  * @LastEditors: tanshaobo
- * @LastEditTime: 2024-03-25 23:25:33
+ * @LastEditTime: 2024-03-26 00:17:01
  * @Description: ts中的函数
  * @FilePath: \T\ts\ts-02.ts
  */
@@ -58,12 +58,16 @@ console.log(f7(1,2,3,5,6,7,8))
 
 /**
  * 重载 函数同名，参数类型不同 
- */
+ * 注：重载应该是想锁死函数处理功能几个重载，意味着至多几种处理结果 且每个重载必须有对应处理逻辑？
+ * 该功能是真的废  
+ * */
+console.log('为了保证上面的注释出现在对应的js文件中')
 
 function f8(name:string):string
-function f8(age:number): string
-function f8(str:any): string {
-    return typeof str === 'string' ? str : '现在' + str
+function f8(sex:number):string
+function f8(name:string, age:number): string
+function f8(name: any, age?:number): string {
+    return !age ? `我是${name}` :  `今年${age}岁` 
 }
 
-console.log(f8(34))
+// console.log(f8(34)) // 错误 因为重载中没有只传数字的形式
